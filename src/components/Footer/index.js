32906a0f3ae8954324ce608637d76d58dd9690d9
Erick Nguyen <erick@loyalter.com>
2019-02-22 12:31:24 -0500

    Add App layout, structure, sample data, react router and use npm going forward

---
+++ src/components/Footer/index.js
@@ -0,0 +1,1 @@
+//This is Footer component
---
+++ src/data-source/contacts/index.js
@@ -0,0 +1,15 @@
+export const FatherTuanTran = {
+  firstname: 'Tuan',
+  lastname: 'Tran',
+  title: 'Fr. Tuan Tran',
+  officePhoneNumber: '7709210077',
+  officePhoneNumberExtension: '1'
+}
+
+export const DecanHoaPham = {
+  firstname: 'Hoa',
+  lastname: 'Pham',
+  title: 'Decan Hoa Pham',
+  officePhoneNumber: '7709210077',
+  officePhoneNumberExtension: '5'
+}

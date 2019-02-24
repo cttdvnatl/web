add6c3d9a192d1d9cb7f9ca66539c85f50c5fe78
Erick Nguyen <erick@loyalter.com>
2019-02-23 19:21:22 -0500

    add basic layout for Home, About, Contact pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-//import PropTypes from 'prop-types';
 
-export default class Contact extends Component {
+export default class About extends Component {
   componentDidMount() {
     //This is to handle login or fetch info
   }
 
   render() {
     return (
-      <h1>Contact Page</h1>
+      <h1>About Page</h1>
     )
   }
 }

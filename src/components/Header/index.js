e3c8560386aa2b7273bfd1f934fe83a634d38e4b
Phuong Tran <phuong.tran@tstllc.net>
2019-02-25 22:44:32 -0500

    Add draft version for Main Menu and Drawer for desktop & mobile

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import MainMenu from '../MainMenu';
+import './styles.css'
+
+const Header = () => {
+  return (
+      <MainMenu />
+  )
+}
+
+export default Header;
